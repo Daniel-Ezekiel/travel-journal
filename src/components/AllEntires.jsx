1597208612bee4f994111b8e6b1a05625c098c72
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-02-22 01:04:41 +0100

    chore: changed styles and added separator between cards

--- src/components/AllEntires.jsx
+++ src/components/AllEntires.jsx
@@ -2,9 +2,9 @@ import Location from './Location';
 import locationsData from '../data.jsx';
 
 const AllEntires = () => {
-  const allLocationEntries = locationsData.map((item, i) => {
-    return <Location key={i + 1} {...item} />;
-  });
+  const allLocationEntries = locationsData.map((item, i, arr) => (
+    <Location key={i + 1} {...item} />
+  ));
   return <section className="all-locations">{allLocationEntries}</section>;
 };
 
